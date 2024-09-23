ec68c6ee91283de3a8fc8a50108259d7747566da
111nation <tafara.k@outlook.com>
2024-09-23 19:32:17 +0200

    Automation compilation script

--- run.bat
+++ run.bat
@@ -4,15 +4,15 @@ if exist *.o (
 	del *.o 
 )
 
-if exist "exec\Game.exe" (
-	del "exec\Game.exe"
-)
-
 g++ -c game.cpp
 g++ -c gamedata.cpp
 g++ -c roomdata.cpp
 g++ -c coindata.cpp
 g++ game.o gamedata.o roomdata.o coindata.o -o "Game.exe"
+
+if exist "Game.exe" (
+	del "Game.exe"
+)
 
 @echo on
 echo .
