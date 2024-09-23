88a23449d7b77a73262d28813b9655f03fca5f70
111nation <tafara.k@outlook.com>
2024-09-23 19:32:42 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-if exist *.o (
-	del *.o 
+if exist "Game.exe" (
+	del "Game.exe"
 )
 
 g++ -c game.cpp
@@ -10,8 +10,8 @@ g++ -c roomdata.cpp
 g++ -c coindata.cpp
 g++ game.o gamedata.o roomdata.o coindata.o -o "Game.exe"
 
-if exist "Game.exe" (
-	del "Game.exe"
+if exist *.o (
+	del *.o 
 )
 
 @echo on
