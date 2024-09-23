3bd9e5957f1854dc32b53fcd73344928807fe9a1
111nation <tafara.k@outlook.com>
2024-09-23 19:31:30 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -12,10 +12,8 @@ g++ -c game.cpp
 g++ -c gamedata.cpp
 g++ -c roomdata.cpp
 g++ -c coindata.cpp
-g++ game.o gamedata.o roomdata.o coindata.o -o "exec\Game.exe"
+g++ game.o gamedata.o roomdata.o coindata.o -o "Game.exe"
 
-if exist "exec\Game.exe" (
-	del *.o
-	cls 
-	"exec\Game.exe"
-)
+@echo on
+echo .
+echo Run the game from Game.exe
