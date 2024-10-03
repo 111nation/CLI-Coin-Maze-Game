e88c3e0f67a12560eb69c9967917de8de5f5fa58
Tafara <tafara.k@outlook.com>
2024-10-03 17:27:44 +0200

    Version 1.2
    Game is playable and includes colouring and improved game story
    dialogue.
    
    Enjoy!

--- compile.bat
+++ compile.bat
@@ -4,7 +4,10 @@ g++ -c game.cpp
 g++ -c gamedata.cpp
 g++ -c roomdata.cpp
 g++ -c coindata.cpp
-g++ game.o gamedata.o roomdata.o coindata.o -o "Game.exe"
+g++ -c minedata.cpp
+g++ -c outputdata.cpp
+g++ -c display.cpp
+g++ display.o game.o gamedata.o roomdata.o coindata.o minedata.o outputdata.o -o "Game.exe"
 
 if exist *.o (
 	del *.o 
